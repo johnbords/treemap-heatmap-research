c55c5b19307f53711a2192d2cbea96ce38bbc5e4
Biboy <johnnephiaquindeb@gmail.com>
2026-02-08 19:56:56 -1000

    Add render_page()

--- view/ui.py
+++ view/ui.py
@@ -1,1 +1,12 @@
 import streamlit as st
+
+def render_page():
+    # init page
+    # The page_title param is for the page's tab title
+    st.set_page_config(page_title="Treemap vs Heatmap", layout="wide")
+
+    # Sets the Page title
+    st.title("Treemap vs Heatmap")
+
+if __name__ == "__main__":
+    render_page()
