97cdc360a3e2650593d5374cf661bcea8df4ec90
Biboy <johnnephiaquindeb@gmail.com>
2026-02-25 00:14:11 -1000

    Add functions to clean and load datasets

--- model/datasets.py
+++ model/datasets.py
@@ -1,4 +1,20 @@
 import pandas as pd
 
+import streamlit as st
+
+@st.cache_data(show_spinner=False)
 def load_dataset(file_name: str) -> pd.DataFrame:
-    return pd.read_csv(file_name)
+    df = pd.read_csv(file_name)
+    df["genre"] = df["genre"].astype(str).str.split(",")
+    df = df.explode("genre")
+    df["genre"] = df["genre"].astype(str).str.strip()
+    df["year"] = pd.to_numeric(df["year"], errors="coerce").astype("Int64")
+    df = df.dropna(subset=["year"])
+    df["year"] = df["year"].astype(int)
+    return df
+
+@st.cache_data(show_spinner=False)
+def load_genre_list(df: pd.DataFrame) -> list:
+    genre_col = df["genre"].astype(str).str.split(",").tolist()
+    genre_list = [g.strip() for genre_set in genre_col for g in genre_set]
+    return sorted(dict.fromkeys(genre_list), key=str.lower)
